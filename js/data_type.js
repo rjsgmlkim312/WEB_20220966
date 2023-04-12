8126bfdfa6b9d057eca38fe350b23e07c08a90a6
rjsgmlkim312 <rjsgmlkim312@naver.com>
2023-04-12 02:13:18 +0000

    6주차 실습

---
+++ js/data_type.js
@@ -0,0 +1,8 @@
+let number = 5;
+let str = '문자열 입력' // ""도 묶음 가능.
+let prime = 1.5123;
+let is_ok = true; // 참
+let is_not = false; // 거짓
+let undefi; // 변수 이름만 초기화 불가능
+let empty = null; // 비어있음
+// consloe.log(undefi,empty); // 여러개 출력 가능.
