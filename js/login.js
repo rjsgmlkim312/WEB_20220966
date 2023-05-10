9fda1a38166a4796b2c6fc46ed6b35f03a59c800
rjsgmlkim312 <rjsgmlkim312@naver.com>
2023-05-10 02:32:28 +0000

    실습 10주차

--- js/login.js
+++ js/login.js
@@ -2,21 +2,48 @@ function login(){
 	let form = document.querySelector("#form_main");
 	let id = document.querySelector("#floatingInput");
 	let passward = document.querySelector("#floatingPassword");
+	let check = document.querySelector("#idSaveCheck");
+
 	
 	form.action = "../index_login.html";
 	form.method = "get";
 	
-	if(id.value.length === 0 || passward.value.length === 0){
-		alert("아이디와 비밀번호를 모두 입력해주세요.");
-	}
-	else{
-		form.submit();
+	if(check.checked == true) { // 아이디 체크 o
+        alert("쿠키를 저장합니다.");
+        setCookie("id", id.value, 1); // 1일 저장
+        alert("쿠키 값 :" + id.value);
+        } 
+    else { // 아이디 체크 x
+        setCookie("id", id.value, 0); //날짜를 0 - 쿠키 삭제
 	}
 }
 
+function setCookie(name, value, expiredays) {
+        var date = new Date();
+        date.setDate(date.getDate() + expiredays);
+        document.cookie = escape(name) + "=" + escape(value) + "; expires=" + date.toUTCString() + "SameSite=None; Secure";       
+}
+
+function getCookie(name) {
+        var cookie = document.cookie;
+        console.log("쿠키를 요청합니다.");
+        if (cookie != "") {
+            var cookie_array = cookie.split("; ");
+            for ( var index in cookie_array) {
+                var cookie_name = cookie_array[index].split("=");
+                
+                if (cookie_name[0] == "id") {
+                    return cookie_name[1];
+                }
+            }
+        }
+        return ;
+}
+	
 function logout(){
 	location.href='../index.html';
 }
+	
 function get_id(){
     var getParameters = function(paramName){ // 변수 = 함수(이름)
     var returnValue; // 리턴값을 위한 변수 선언
@@ -33,4 +60,22 @@ function get_id(){
 	    } // 2중 for문 끝
 }; // 함수 끝
 alert(getParameters('id') + '님 방갑습니다!'); // 메시지 창 출력
+}
+
+
+function deleteCookie(cookieName){
+    var expireDate = new Date();
+    expireDate.setDate(expireDate.getDate() - 1);
+    document.cookie = cookieName + "= " + "; expires=" + expireDate.toGMTString();
+}
+
+function init(){ // 로그인 폼에 쿠키에서 가져온 아이디 입력
+    let id = document.querySelector("#floatingInput");
+    let check = document.querySelector("#idSaveCheck");
+    let get_id = getCookie("id");
+    
+    if(get_id) { 
+    id.value = get_id; 
+    check.checked = true; 
+    }
 }
