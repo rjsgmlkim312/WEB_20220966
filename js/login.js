3eff5fbc092c21c8c0b309bc6fdb8b8fc4ef9461
rjsgmlkim312 <rjsgmlkim312@naver.com>
2023-05-17 02:30:44 +0000

    12주차 실습

--- js/login.js
+++ js/login.js
@@ -16,6 +16,14 @@ function login(){
     else { // 아이디 체크 x
         setCookie("id", id.value, 0); //날짜를 0 - 쿠키 삭제
 	}
+	
+	if(id.value.length === 0 || passward.value.length === 0){
+		alert("아이디와 비밀번호를 입력해주세요.")
+	}
+	else{
+		session_set(); // 세션 생성
+		form.submit();
+	}
 }
 
 function setCookie(name, value, expiredays) {
@@ -31,7 +39,6 @@ function getCookie(name) {
             var cookie_array = cookie.split("; ");
             for ( var index in cookie_array) {
                 var cookie_name = cookie_array[index].split("=");
-                
                 if (cookie_name[0] == "id") {
                     return cookie_name[1];
                 }
@@ -41,25 +48,30 @@ function getCookie(name) {
 }
 	
 function logout(){
+	session_del();
 	location.href='../index.html';
 }
 	
 function get_id(){
+	/*if(true){
+        decrypt_text();
+    }
+    else{*/
     var getParameters = function(paramName){ // 변수 = 함수(이름)
     var returnValue; // 리턴값을 위한 변수 선언
     var url = location.href; // 현재 접속 중인 주소 정보 저장
     var parameters = (url.slice(url.indexOf('?') + 1, url.length)).split('&'); // ?기준 slice 한 후 split 으로 나눔
         for(var i = 0; i < parameters.length; i++) { 
 		    var varName = parameters[i].split('=')[0];
-            
             if (varName.toUpperCase() == paramName.toUpperCase()) {
                 returnValue = parameters[i].split('=')[1];
                 return decodeURIComponent(returnValue);
             // 나누어진 값의 비교를 통해 paramName 으로 요청된 데이터의 값만 return
 		    }
 	    } // 2중 for문 끝
-}; // 함수 끝
-alert(getParameters('id') + '님 방갑습니다!'); // 메시지 창 출력
+	}; // 함수 끝
+	alert(getParameters('id') + '님 방갑습니다!'); // 메시지 창 출력
+	setTimeout(logout,date.getMinutes + 5);
 }
 
 
@@ -73,9 +85,90 @@ function init(){ // 로그인 폼에 쿠키에서 가져온 아이디 입력
     let id = document.querySelector("#floatingInput");
     let check = document.querySelector("#idSaveCheck");
     let get_id = getCookie("id");
-    
     if(get_id) { 
     id.value = get_id; 
     check.checked = true; 
     }
+	session_check(); // 세션 유무 검사
 }
+
+function session_set(){
+	    let id = document.querySelector("#floatingInput");
+    if (sessionStorage) {
+        sessionStorage.setItem("Session_Storage_test", id.value);
+	/*let id = document.querySelector("#floatingInput");
+	let password = document.querySelector("#floatingPassword");
+    if (sessionStorage) {
+        let en_text = encrypt_text(password.value);
+        sessionStorage.setItem("Session_Storage_test", en_text);
+	*/
+	}
+	else{
+		alert("로컬 스토리지 지원 x");
+	}
+}
+
+
+function session_get(){
+	if(sessionStorage){
+		return sessionStorage.getItem("Session_Storage_test");
+	}
+	else{
+		alert("세션 스토리지 지원 x")
+	}
+}
+
+function session_check(){
+	if(sessionStorage.getItem("Session_Storage_test")){
+		alert("이미 로그인 되어 있습니다.");
+		location.href ='index_login.html'; // 로그인된 페이지로 이동
+	}
+}
+
+function session_del(){
+	// check if the sessionStorage object exists
+	if(sessionStorage){
+		//Retrive data
+		sessionStorage.removeItem("Session_Storage_test");
+		alert("로그아웃 버튼 클릭 확인 : 세션 스토리지를 삭제합니다.");
+	}
+	else {
+		alert(" 세션 스토리지 지원 x");
+	}
+}
+/*
+function encodeByAES256(key, data){
+    const cipher = CryptoJS.AES.encrypt(data, CryptoJS.enc.Utf8.parse(key), {
+        iv: CryptoJS.enc.Utf8.parse(""),
+        padding: CryptoJS.pad.Pkcs7,
+        mode: CryptoJS.mode.CBC
+    });
+    return cipher.toString();
+}
+
+function decodeByAES256(key, data){
+    const cipher = CryptoJS.AES.decrypt(data, CryptoJS.enc.Utf8.parse(key), {
+        iv: CryptoJS.enc.Utf8.parse(""),
+        padding: CryptoJS.pad.Pkcs7,
+        mode: CryptoJS.mode.CBC
+    });
+    return cipher.toString(CryptoJS.enc.Utf8);
+}
+
+function encrypt_text(password){
+    const k = "key"; // 클라이언트 키
+    const rk = k.padEnd(32, " "); // AES256은 key 길이가 32
+    const b = password;
+    const eb = this.encodeByAES256(rk, b);
+    return eb;
+    console.log(eb);
+}
+
+function decrypt_text(){
+    const k = "key"; // 서버의 키
+    const rk = k.padEnd(32, " "); // AES256은 key 길이가 32
+    const eb = session_get();
+    const b = this.decodeByAES256(rk, eb);
+    console.log(b); 
+}
+*/
